1bc3761f794c7f805623d05668151f644d3d0d41
Kent Sutherland <git@ksuther.com>
2018-07-05 13:50:52 -0500

    Copy config from /config/config.ini.php to avoid read-only files when mounting a Kubernetes configmap

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ ! -e piwik.php ]; then
 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
+    cp /config/config.ini.php /var/www/html/config/config.ini.php
 	chown -R www-data .
 fi
 
